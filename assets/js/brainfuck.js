0ca30da583688c7facfa29db7e28c6b00813b498
Steven Imle (MacPro) <stevenimle@gmail.com>
2015-10-01 15:25:47 -0500

    Added more stuff

--- assets/js/brainfuck.js
+++ assets/js/brainfuck.js
@@ -1,5 +1,18 @@
+Util = {
+	parse: {
+		template: function(template, data) {
+			return template.replace(/%(\w+)%/g, function (m, key) {
+				return data.hasOwnProperty(key) ? data[key] + "" : "";
+			});
+		}
+	}
+};
+
 Brainfuck = {
-	initialize: function() {},
+	array_size: 120,
+	initialize: function() {
+		Brainfuck.GUI.initialize();
+	},
 	GUI: {
 		elements: {
 			template: "",
@@ -9,6 +22,19 @@ Brainfuck = {
 			Brainfuck.GUI.elements.template = $("#template_bf_column").html();
 
 			Brainfuck.GUI.elements.$contianer = $("#brainfuck_container");
+
+			var html = "";
+			for (var i = 0; i < Brainfuck.array_size; i++) {
+				html += Util.parse.template(Brainfuck.GUI.elements.template, {
+					number: i
+				});
+			}
+
+			Brainfuck.GUI.elements.$contianer.html(html);
 		}
 	}
 };
+
+$(document).ready(function() {
+	Brainfuck.initialize();
+});
